92e4e15e9c6ffe129f63bbde350c30d0dfdf2384
Your GitHub Username <Your GitHub Email>
2026-07-06 20:54:13 +0600

    admin id create and get all user

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,14 +3,26 @@ import { registerUser, loginUser } from '../controllers/auth.controllers';
 import { createPaymentIntent, confirmPayment } from '../controllers/payment.controllers';
 import { authGuard, checkRole } from '../middleware/auth';
 
+import {
+  getAllUsers,
+  toggleUserStatus,
+  getAllPropertiesForAdmin,
+  getAllRentalsForAdmin,
+} from '../controllers/admin.controller';
+
 const router = Router();
 
-// Auth Routes
+
 router.post('/auth/register', registerUser);
 router.post('/auth/login', loginUser);
 
-// Payment Routes
+
 router.post('/payments/create', authGuard, checkRole('TENANT'), createPaymentIntent);
 router.post('/payments/confirm', authGuard, checkRole('TENANT'), confirmPayment);
+
+router.get('/admin/users', authGuard, checkRole('ADMIN'), getAllUsers);
+router.patch('/admin/users/:id', authGuard, checkRole('ADMIN'), toggleUserStatus);
+router.get('/admin/properties', authGuard, checkRole('ADMIN'), getAllPropertiesForAdmin);
+router.get('/admin/rentals', authGuard, checkRole('ADMIN'), getAllRentalsForAdmin);
 
 export default router;
