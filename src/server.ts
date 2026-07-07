f4072b82d88a62bb9eb27bf38c382b7327f84694
Your GitHub Username <Your GitHub Email>
2026-07-07 12:13:16 +0600

    server port running problem

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import app from './app';
+// import app from './app';
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-async function bootstrap() {
-  app.listen(PORT, () => {
-    console.log(` RentNest Server is running on port ${PORT}`);
-  });
-}
+// async function bootstrap() {
+//   app.listen(PORT, () => {
+//     console.log(` RentNest Server is running on port ${PORT}`);
+//   });
+// }
 
-bootstrap();
+// bootstrap();
